b54c32ced1ba5b3a3a10e4a7314328f5911b5b02
Roger D. Peng <rpeng@jhsph.edu>
2010-04-12 00:00:00 +0000

    version 0.1-6

--- R/zzz.R
+++ R/zzz.R
@@ -15,7 +15,7 @@
         dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
         msg <- gettextf("%s (%s %s)", dcf[, "Title"],
                         as.character(dcf[, "Version"]), dcf[, "Date"])
-        message(paste(strwrap(msg), collapse = "\n"))
+        packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
 }
 
 .configEnv <- new.env()
